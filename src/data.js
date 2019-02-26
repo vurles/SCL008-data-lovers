8a3ee9ebe2b0b8f230926ff7c98a05cca89104d2
vurles <vurles27@gmail.com>
2019-02-26 20:06:27 -0300

    union de main con daja a traves de la funcion filterPokemon que proviene del select

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-/* Manejo de data */
+/* Manejo de data 
 
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
@@ -7,4 +7,15 @@ const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+
+
+
+const filterPokemon = (DATA, condition) => { //data ? 
+  let filteredPoke = DATA.filter(element => { // filter recorre y arroja un nuevo array 
+    
+    return element.type.includes(condition)// includes funciona con booleanos compara si la condicion se cumple
+  })
+  return filteredPoke;
+};
+window.filterPokemon = filterPokemon()
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 /* Manejo del DOM */
-const DATA = POKEMON.pokemon;//me traigo la data de pokemon y la guargo en una const
+const data = POKEMON.pokemon;//me traigo la data de pokemon y la guargo en una const
 //console.log(DATA);
 const pokebox = document.getElementById('root'); // creo una constatante para llammar a mi contenedor root
 
-const seeData = (DATA) => {
+const pokeType = document.getElementById('type'); // llamo a mi select para poder filtrar por el valor que me da
+
+
+
+const seeData = (data) => {
 
     let result = '';
-    DATA.forEach(element => {//element es === a DAATA[i]
+    data.forEach(element => {//element es === a DAATA[i]
         result = pokebox.innerHTML += `
         <div class="carta-box">
                     <div class="carta">    
@@ -24,7 +28,36 @@ const seeData = (DATA) => {
                     </div>
                   </div>
         `
+
     });
     return result;
 }
-window.onload = seeData(DATA);
+pokeType.addEventListener('change', () => {
+    let condition = pokeType.value
+    let filtered = filterPokemon(data, condition);
+    // limpiando el div
+    pokebox.innerHTML = '';
+    filtered.forEach(element => {
+        pokebox.innerHTML += `
+        <div>
+        <div class="card">
+            <div class="box">
+                <h3 class="tituloPoke" >
+                ${element.name}
+                </h3>
+                <div class="img">
+                    <img src="${element.img}">
+                </div>
+                <p> Tipo: ${element.type}</p>
+                <p>Debilidad: ${element.weaknesses}</p>
+            </div>
+        </div>
+    </div>
+
+        `
+    })
+})
+
+
+
+window.onload = seeData(data);
