12ef039f3cc82590f9cf21f7b94c16c39e081dbf
vurles <vurles27@gmail.com>
2019-02-27 11:41:22 -0300

    se cambia el formato de las cartas  al  mostar la data cuando filtra

--- src/main.js
+++ src/main.js
@@ -39,20 +39,21 @@ pokeType.addEventListener('change', () => {
     pokebox.innerHTML = '';
     filtered.forEach(element => {
         pokebox.innerHTML += `
-        <div>
-        <div class="card">
-            <div class="box">
-                <h3 class="tituloPoke" >
-                ${element.name}
-                </h3>
-                <div class="img">
-                    <img src="${element.img}">
-                </div>
-                <p> Tipo: ${element.type}</p>
-                <p>Debilidad: ${element.weaknesses}</p>
-            </div>
+        <div class="carta-box">
+        <div class="carta">    
+          <div class="cara">
+          <h3 class="tituloPoke" >
+          ${element.name}
+          </h3>
+            <img src="${element.img}">
+          </div>
+          <div class="cara detras">
+            <p>${element.type}</p>
+            <p>${element.weaknesses}</p>
+            <p>${element.candy}</p>
+          </div>    
         </div>
-    </div>
+      </div>
 
         `
     })
